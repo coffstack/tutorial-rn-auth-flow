bd5594e7de7215fdffb9078a775bb374cdff56f4
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-01-24 21:36:13 -0300

    update loading message

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {AppStack} from './AppStack';
 import {AuthStack} from './AuthStack';
 import {useAuth} from '../contexts/Auth';
 
-const Stack = createNativeStackNavigator();
 export function Router() {
   const {authData, isLoading} = useAuth();
 
@@ -15,7 +13,7 @@ export function Router() {
     console.log({isLoading});
     return (
       <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-        <Text>Carregando</Text>
+        <Text>Carregando informações....</Text>
       </View>
     );
   }
