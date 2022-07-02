cda31d1f2525de2b64aeec63c475ed1b08903ba8
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 09:21:11 -0300

    theme files

---
+++ src/theme/darkTheme.ts
@@ -0,0 +1,12 @@
+import {DefaultTheme} from 'styled-components';
+export const darkTheme: DefaultTheme = {
+  colors: {
+    background: '#121212',
+    backgroundSecondary: '#333',
+    onBackground: '#FFF',
+    primary: '#550AB1',
+  },
+  space: {
+    default: 16,
+  },
+};
---
+++ src/theme/lightTheme.ts
@@ -0,0 +1,12 @@
+import {DefaultTheme} from 'styled-components';
+export const lightTheme: DefaultTheme = {
+  colors: {
+    background: '#FFF',
+    backgroundSecondary: '#EEE',
+    onBackground: '#121212',
+    primary: '#1D013F',
+  },
+  space: {
+    default: 16,
+  },
+};
