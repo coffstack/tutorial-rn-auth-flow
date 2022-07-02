59245b8ac31c4ba9956a2edd999bca27ea023d22
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 10:43:52 -0300

    toggleTheme

--- src/screens/SignInScreen/SignInScreen.tsx
+++ src/screens/SignInScreen/SignInScreen.tsx
@@ -1,21 +1,26 @@
-import React, {useState} from 'react';
-import {View, Image} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {Image, Switch} from 'react-native';
 import {MyButton} from '../../components/MyButton';
 import {MyTextInput} from '../../components/MyTextInput';
 import {useAuth} from '../../contexts/Auth';
-import {styles} from '../styles';
 
 import logo from '../../assets/logo.png';
 
 import {Container} from './styles';
+import {ThemeContext, ThemeType} from '../../theme/Theme';
 
 export function SignInScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const {signIn} = useAuth();
+  const {toggleTheme, theme} = useContext(ThemeContext);
+
+  const darkModeIsEnabled = theme === ThemeType.dark;
 
   return (
     <Container>
+      <Switch value={darkModeIsEnabled} onValueChange={toggleTheme} />
+
       <Image
         resizeMode="contain"
         source={logo}
