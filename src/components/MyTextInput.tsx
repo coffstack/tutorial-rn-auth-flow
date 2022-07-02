65be7258c83a2709b63a76b33dbfd6bef574eb1d
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 09:20:47 -0300

    text input with theme

--- src/components/MyTextInput.tsx
+++ src/components/MyTextInput.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {StyleSheet, TextInput, TextInputProps} from 'react-native';
+import {useTheme} from 'styled-components/native';
 
 export function MyTextInput(props: TextInputProps) {
+  const {colors} = useTheme();
   return (
-    <TextInput placeholderTextColor="#727272" style={styles.input} {...props} />
+    <TextInput
+      placeholderTextColor="#727272"
+      style={[
+        styles.input,
+        {borderColor: colors.primary, color: colors.onBackground},
+      ]}
+      {...props}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   input: {
     borderRadius: 8,
-    borderColor: '#1D013F',
     paddingHorizontal: 8,
-    color: '#000',
     borderWidth: 1,
     width: '100%',
     height: 50,
