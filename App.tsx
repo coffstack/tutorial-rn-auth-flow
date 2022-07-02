9a2e644bd667166b14d37f1e8242ee1ba4d04154
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 12:25:52 -0300

    ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components/native';
 import {AuthProvider} from './src/contexts/Auth';
 import {Router} from './src/routes/Routes';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
+    <ThemeProvider
+      theme={{
+        colors: {
+          background: '#121212',
+          onBackground: '#FFF',
+          primary: '#550AB1',
+        },
+        spaces: {
+          default: 16,
+        },
+      }}>
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
