4a45ece49a9c3af4a31ba338b13574c312bb5fe0
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 09:21:31 -0300

    AuthStack theme

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {SignInScreen} from '../screens/SignInScreen';
+import {SignInScreen} from '../screens/SignInScreen/SignInScreen';
+import {useTheme} from 'styled-components';
 
 const Stack = createNativeStackNavigator();
 
 export function AuthStack() {
+  const {colors} = useTheme();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: colors.onBackground,
+        headerStyle: {
+          backgroundColor: colors.backgroundSecondary,
+        },
+      }}>
       <Stack.Screen name="SignIn" component={SignInScreen} />
     </Stack.Navigator>
   );
